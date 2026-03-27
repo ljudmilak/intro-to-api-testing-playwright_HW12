c9505cbeee3520b75dabfebf9b0761325434a95a
ljudmila.kend <ljudmila.kend@gmail.com>
2026-03-27 17:30:51 +0200

    Initial commit

---
+++ src/dto/ProductDTO.ts
@@ -0,0 +1,31 @@
+export class ProductDTO {
+  id: number
+  name: string
+  price: number
+  createdAt: string | null
+
+  constructor(id: number, name: string, price: number, createdAt: string | null) {
+    this.id = id
+    this.name = name
+    this.price = price
+    this.createdAt = createdAt
+  }
+
+  static generateDefault(): ProductDTO {
+    return new ProductDTO(
+      0,
+      'home work 12',
+      100,
+      new Date().toISOString()
+    )
+  }
+
+  static generateCustom(name: string, price: number): ProductDTO {
+    return new ProductDTO(
+      0,
+      name,
+      price,
+      new Date().toISOString()
+    )
+  }
+}
