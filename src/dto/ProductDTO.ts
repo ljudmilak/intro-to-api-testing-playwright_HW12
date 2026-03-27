80650627d359abae1981d2d3217039b011aa9eb6
ljudmila.kend <ljudmila.kend@gmail.com>
2026-03-27 19:42:10 +0200

    change

--- src/dto/ProductDTO.ts
+++ src/dto/ProductDTO.ts
@@ -12,7 +12,7 @@ export class ProductDTO {
   }
 
   static generateDefault(): ProductDTO {
-    return new ProductDTO(0, 'home work 12', 100, new Date().toISOString())
+    return new ProductDTO(0, 'home work 12', 1000, new Date().toISOString())
   }
 
   static generateCustom(name: string, price: number): ProductDTO {
